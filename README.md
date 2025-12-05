1946647d68eb2a050e7a0980c88290c2b91a9989
Physis21 <juliendudas@gmail.com>
2025-12-05 09:46:25 +0100

    feat: add misc plugins, and correctly setup catppuccin colortheme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Physis21 Neovim config
+
+- This config has been created following [this youtube video](https://youtu.be/KYDG3AHgYEs?si=XzmA9sXohIXH1aRv), which itself is heavily influenced by kickstart.
--- init.lua
+++ init.lua
@@ -1,20 +1,20 @@
-require("core.options")
-require("core.keymaps")
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
-end
+require("core.options") -- Load general options
+require("core.keymaps") -- Load general keymaps
 
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+-- Set up the Lazy plugin manager
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
 -- Define plugins
-plugins = {
-	require("plugins.autocompletion"),
+
+require("lazy").setup {
 	require("plugins.neotree"),
 	require("plugins.colortheme"),
 	require("plugins.bufferline"),
@@ -22,8 +22,11 @@ plugins = {
 	require("plugins.treesitter"),
 	require("plugins.telescope"),
 	require("plugins.lsp"),
+	require("plugins.autocompletion"),
 	require("plugins.none-ls"),
-    require("plugins.gitsigns"),
+	require("plugins.gitsigns"),
+	require("plugins.alpha"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+	require("plugins.comment"),
 }
-
-require("lazy").setup(plugins)
