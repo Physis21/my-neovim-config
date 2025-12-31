787cf25a131df37de45953cf3ca27d0d3daf6474
Physis21 <juliendudas@gmail.com>
2025-12-31 15:42:17 +0100

    feat: add <c-s> save in insert mode

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,6 +10,7 @@ local opts = { noremap = true, silent = true }
 
 -- Save file
 vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", opts)
+vim.keymap.set("i", "<C-s>", "<cmd> w <CR>", opts)
 
 -- Quit file
 vim.keymap.set("n", "<C-q>", "<cmd> q <CR>", opts)
