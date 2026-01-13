c3da4835ecdd3c295ec4703a1bc657ceefe66785
Physis21 <juliendudas@gmail.com>
2026-01-13 10:43:48 +0100

    feat: add comments and use html and css lsp

--- init.lua
+++ init.lua
@@ -1,15 +1,28 @@
 require("core.options") -- Load general options
 require("core.keymaps") -- Load general keymaps
 
--- Set up the Lazy plugin manager
+-- Set up the Lazy plugin manager.
+-- The vim.fn wrapper is the only way to access vimscript functions from lua.
+-- stdpath("data") persistently stores the data between neovim sessions.
+-- '..' is the concatenation operator
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable", -- latest stable relase
+    lazyrepo,
+    lazypath, -- Lazy is installed into the lazypath
+  })
   if vim.v.shell_error ~= 0 then
     error("Error cloning lazy.nvim:\n" .. out)
   end
 end
+-- rtp = runtime path
+-- This is where neovim looks when it has a question that it can't answer itself.
+-- 'prepend' means nvim will look at lazypath first in the runtime path.
 vim.opt.rtp:prepend(lazypath)
 
 -- Define plugins
