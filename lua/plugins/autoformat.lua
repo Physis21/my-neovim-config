93a3c0314d80b983e4364aca0facdd25ae682a46
Physis21 <juliendudas@gmail.com>
2026-01-17 09:32:20 +0100

    feat: add html autoformat in conform plugin

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -33,6 +33,10 @@ return { -- Autoformat
       -- Conform can also run multiple formatters sequentially
       python = { "isort", "black" },
       cpp = { "clang-format" },
+      html = { "djlint", "html_beautify" },
+      css = { "stylelint" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
